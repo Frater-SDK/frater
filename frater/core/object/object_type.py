efe17ae61b46818ebefd9afef81c6e27443e7ecf
John Henning <john.l.henning@ibm.com>
2019-05-29 22:15:08 -0400

    added tree type to frater

--- frater/core/object/object_type.py
+++ frater/core/object/object_type.py
@@ -1,22 +1,23 @@
 from enum import Enum
 
 __long_names__ = ['Null', 'Person', 'Vehicle', 'Door', 'Prop',
-                  'Push_Pulled_Object', 'Other', 'Bike', 'Parking_Meter']
+                  'Push_Pulled_Object', 'Other', 'Bike', 'Parking_Meter', 'Tree']
 
 
 class ObjectType(Enum):
     """
     Object Types for DIVA
     """
-    NULL = 0          # Null
-    PERSON = 1        # Person
-    VEHICLE = 2       # Vehicle
-    DOOR = 3          # Door
-    PROP = 4          # Prop
+    NULL = 0  # Null
+    PERSON = 1  # Person
+    VEHICLE = 2  # Vehicle
+    DOOR = 3  # Door
+    PROP = 4  # Prop
     PULLED_OBJECT = 5  # Pushed_Pulled_Object
-    OTHER = 6         # Other
-    BIKE = 7          # Bike
+    OTHER = 6  # Other
+    BIKE = 7  # Bike
     PARKING_METER = 8  # Parking_Meter
+    TREE = 9
 
     # sub groups
     TRANSPORT_OBJECTS = {PROP, PULLED_OBJECT, OTHER}
