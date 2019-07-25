54821d123ec142b131be7014f9f73950daa4eb09
John Henning <john.l.henning@ibm.com>
2019-07-25 12:46:51 -0400

    fixed divide by zero bug

--- frater/core/trajectory/trajectory_functions.py
+++ frater/core/trajectory/trajectory_functions.py
@@ -6,4 +6,6 @@ __all__ = ['compute_spatiotemporal_iou']
 def compute_spatiotemporal_iou(trajectory: Trajectory, other_trajectory: Trajectory) -> float:
     intersection = trajectory.intersect(other_trajectory).volume()
     union = trajectory.volume() + other_trajectory.volume() - intersection
+    if union <= 0:
+        return 0.0
     return intersection / union
