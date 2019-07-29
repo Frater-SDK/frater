ae730f290e9b4faf817439f0bd5f45b9a95d4b0b
John Henning <john.l.henning@ibm.com>
2019-07-29 00:12:14 -0400

    fixed bug with filestore

--- frater/data_store/file_store/file_store.py
+++ frater/data_store/file_store/file_store.py
@@ -7,6 +7,5 @@ class FileStore(metaclass=Singleton):
     def __init__(self, root):
         self.root = os.path.abspath(os.path.expandvars(os.path.expanduser(root)))
 
-    def get_full_path(self, image_path):
-        full_path = os.path.join(self.root, image_path)
-        return full_path
+    def get_full_path(self, file_path):
+        return os.path.join(self.root, file_path)
