156444b354f3d70ecfcd7e5d9bda0a718f80af5e
John Henning <john.l.henning15@gmail.com>
2020-02-27 15:17:13 -0500

    refactored test to use flask test request context

--- tests/unit/server/test_middleware.py
+++ tests/unit/server/test_middleware.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, Mock
+from unittest.mock import Mock
 
 import flask
 
@@ -12,13 +12,14 @@ def get_mock_func():
     return mock_func
 
 
-def get_context():
-    return flask.Flask(__name__).app_context()
+def get_app():
+    return flask.Flask(__name__)
 
 
 def mock_middleware(handler):
     def wrapper():
         res = handler()
+
         res['other'] = 1
         return res
 
@@ -30,38 +31,29 @@ def test_reduce_middleware():
     reduced = reduce_middleware(middleware)
 
     mock_func = get_mock_func()
-
     assert reduced(mock_func)() == mock_func()
 
     mock_func.reset_mock()
-
-    ctx = get_context()
-    with ctx:
-        with patch('frater.server.middleware.request') as request:
-            middleware = [mock_middleware, response_middleware]
-            reduced = reduce_middleware(middleware)
-            request.method.return_value = 'GET'
-            res = reduced(mock_func)()
-            assert res.json == {'test': 1234, 'other': 1}
-            assert res.status_code == 200
+    reduced = reduce_middleware([mock_middleware])
+    assert reduced(mock_func)() == {'test': 1234, 'other': 1}
 
     mock_func.reset_mock()
-
-    reduced = reduce_middleware([mock_middleware])
-
-    assert reduced(mock_func)() == {'test': 1234, 'other': 1}
+    app = get_app()
+    middleware = [mock_middleware, response_middleware]
+    with app.test_request_context():
+        reduced = reduce_middleware(middleware)
+        res = reduced(mock_func)()
+        assert res.json == {'test': 1234, 'other': 1}
+        assert res.status_code == 200
 
 
 def test_response_middleware():
     mock_func = get_mock_func()
-    ctx = get_context()
-
-    with ctx:
-        with patch('frater.server.middleware.request') as request:
-            request.method.return_value = 'GET'
-            func = response_middleware(mock_func)
-            assert func().json == {"test": 1234}
-            assert func().status_code == 200
+    app = get_app()
+    with app.test_request_context():
+        res = response_middleware(mock_func)()
+        assert res.json == {'test': 1234}
+        assert res.status_code == 200
 
 
 def test_identity_middleware():
@@ -75,11 +67,9 @@ def test_get_default_middleware():
     middleware = get_default_middleware()
 
     mock_func = get_mock_func()
-    ctx = get_context()
 
-    with ctx:
-        with patch('frater.server.middleware.request') as request:
-            request.method.return_value = 'GET'
-            func = middleware(mock_func)
-            assert func().json == {"test": 1234}
-            assert func().status_code == 200
+    app = get_app()
+    with app.test_request_context():
+        res = middleware(mock_func)()
+        assert res.json == {'test': 1234}
+        assert res.status_code == 200
