325447bbd9d704f41531436a534c7663bc2e2f55
John Henning <john.l.henning15@gmail.com>
2020-03-10 20:13:02 -0400

    added functionality to dataset

--- frater/dataset/dataset.py
+++ frater/dataset/dataset.py
@@ -11,8 +11,15 @@ class Dataset(DataClassJsonMixin):
     name: str = ''
     labels: List[str] = field(default_factory=list)
 
+    def __getitem__(self, item: int):
+        return self.get_category(item)
+
     def __len__(self):
         return len(self.labels)
 
     def get_category(self, index):
         return Category(index, self.labels[index], self.name)
+
+    def get_category_by_label(self, label):
+        index = self.labels.index(label)
+        return self.get_category(index)
